3a751d3d9f325468a4d9ea3f1bf27b0277de9c4e
tomzy-ba <trbee100@gmail.com>
2024-01-08 19:54:14 +0000

    added main part of script

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,2 +1,19 @@
+// name of function
+function getComputerChoice() {
+    // start of function, set randomnum to random value between 1 and 0
+    let randomnum = Math.random()
+    // if less than 1/3, log rock
+    if (randomnum < 1/3) {
+        console.log("rock");
+    }
 
-function getComputerChoice() {}
+    else if (randomnum > 2/3) {
+        console.log("paper");
+    }
+
+    else {
+        console.log("scissors");
+    }
+}
+
+console.log(getComputerChoice)
